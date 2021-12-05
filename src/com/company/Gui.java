7816d5a9c5b5b232509b650a28a2a6f9c884703c
samuelru3 <samuelruebel@gmail.com>
2021-12-05 23:00:33 +0100

    commit

--- src/com/company/Gui.java
+++ src/com/company/Gui.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import org.apache.commons.io.FileUtils;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 
 public class Gui {
     private JButton buttonBereinigen;
@@ -15,7 +18,11 @@ public class Gui {
         buttonBereinigen.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                DeleteDir.deleteDir(new File("C:\\Users\\ATV-Admin\\Downloads"));
+                try {
+                    FileUtils.cleanDirectory(new File("C:\\Users\\ATV-Admin\\Downloads"));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         });
         abbrechenButton.addActionListener(new ActionListener() {
