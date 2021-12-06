00fc8000006efc39e9e18fc21fecf10557461ace
samuelru3 <samuelruebel@gmail.com>
2021-12-06 04:14:59 +0100

    fenster zentriert

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,19 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        ImageIcon icon = new ImageIcon("src/com/company/besenIcon.jpg");
+        ImageIcon icon = new ImageIcon("C:\\Users\\ATV-Admin\\IdeaProjects\\BereinigenTool\\src\\com\\company\\besenIcon.jpg");
         JFrame frame = new JFrame("PC Bereinigen");
         frame.setContentPane(new Gui().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setIconImage(icon.getImage());
 
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
